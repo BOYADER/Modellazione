eb7e56b1ca0a1b3578e701cb0eea15189fbdb10d
riccardo <riccardodelmedico1@gmail.com>
2021-05-18 18:06:22 +0200

    modificato Vector3f

--- src/ahrs.cpp
+++ src/ahrs.cpp
@@ -5,24 +5,33 @@
 #include "geometry_msgs/Vector3.h"
 #include "modellazione/state_real.h"
 #include "modellazione/ahrs.h"
+#include <random>
+
+using namespace Eigen;
+using namespace std;
 
 modellazione::ahrs ahrs_measure;
-geometry_msgs::Vector3 old_pos;
-geometry_msgs::Vector3 old_vel;
-geometry_msgs::Vector3 new_vel;
-geometry_msgs::Vector3 new_pos;
+Vector3f old_pos(0,0,0);
+Vector3f old_vel(0,0,0);
+Vector3f new_vel(0,0,0);
+Vector3f new_pos(0,0,0);
 
 void ahrs_state_read(const modellazione::state_real& state){
   ahrs_measure.rpy = state.eta_2;
   ahrs_measure.gyro = state.ni_2;
-  new_pos = state.eta_1;
+  new_pos(1) = state.eta_1.x;
+  new_pos(2) = state.eta_1.y;
+  new_pos(3) = state.eta_1.z;
   
 }
-
 void compute_measure(){
-  pos_dot = (new_pos-old_pos)*SENSOR_FREQUENCY;
+
+  new_vel = (new_pos-old_pos);
   old_pos = new_pos;
-  ahrs_measure.acc = (new_vel - old_vel)*SENSOR_FREQUENCY;
+  Vector3f acc = (new_vel - old_vel);
+  ahrs_measure.acc.x = acc(1);
+  ahrs_measure.acc.y = acc(2);
+  ahrs_measure.acc.z = acc(3);
   old_vel = new_vel;
 }
 
@@ -39,8 +48,8 @@ int main(int argc, char **argv){
   default_random_engine generator;
   normal_distribution<double> rp_distribution(0, 0.3);    //[deg]
   normal_distribution<double> y_distribution(0, 1);       //[deg]
-  normal_distribution<double> gyro_distribution(0, NULL); //incognita
-  normal_distribution<double> acc_distribution(0, NULL);  //incognita
+  normal_distribution<double> gyro_distribution(0, 0); //incognita
+  normal_distribution<double> acc_distribution(0, 0);  //incognita
 
   ros::Rate loop_rate(SENSOR_FREQUENCY);
   ros::spinOnce();
--- src/constant.h
+++ src/constant.h
@@ -1,14 +1,25 @@
 
 /* PARAMETRI DEL ROBOT */
-#define R_A 0.525		//semiasse maggiore    								[m]
+#define R_A 0.5218		//semiasse maggiore    								[m]
 #define R_B 0.15		//semiasse minore_1 								[m]
 #define R_C 0.15		//semiasse minore_2 								[m]
 #define R_M 50		//massa												[kg]
 #define R_D_GB 0.07 	//distanza tra centro geometrico e centro di massa	[m]
 
 /*PARAMETRI MATRICI DI CORIOLIS E DI DAMPING */
+
+// parametri tensore di inerzia
+#define I_X 0.695
+#define I_Y 3.1928
+#define I_Z 2.9478
+
+#define E 1
+
+
+
 /*
-#define X_u
+
+#define X_u 
 #define X_u_dot
 #define Y_v  
 #define Y_v_dot
@@ -16,7 +27,6 @@
 #define Z_w_dot
 //#define 
 
-*/
 
 
 
