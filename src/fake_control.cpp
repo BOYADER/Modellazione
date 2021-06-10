e905fd6c962293da942b6de961e92ab13000edbb
nicco <niccogazza@hotmail.it>
2021-06-10 17:11:31 +0200

    Deleted config.yaml & fake_control ready to validation

--- src/fake_control.cpp
+++ src/fake_control.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
-#include "/usr/include/eigen3/Eigen/Dense"
 #include "ros/ros.h"
 #include "std_msgs/String.h"
+#include "/usr/include/eigen3/Eigen/Dense"
 #include <geometry_msgs/Vector3.h>
 #include "modellazione/state_real.h"
 #include "modellazione/tau.h"
@@ -13,13 +13,14 @@
 
 
 modellazione::tau control_tau;
+
 void init_tau (){
-control_tau.tau.force.x = 0;
-control_tau.tau.force.y = 0;
-control_tau.tau.force.z = 0;
-control_tau.tau.torque.x = 0;
-control_tau.tau.torque.y = 0;
-control_tau.tau.torque.z = 0;
+  control_tau.tau.force.x = 0;
+  control_tau.tau.force.y = 0;
+  control_tau.tau.force.z = 0;
+  control_tau.tau.torque.x = 0;
+  control_tau.tau.torque.y = 0;
+  control_tau.tau.torque.z = 0;
 }
 
 
@@ -40,16 +41,47 @@ int main(int argc, char **argv)
     
     ros::spinOnce();
 
-    if(count > 25)
+    /*-------------------------SIMULAZIONE IMPULSO---------------------------*/
+    //INSERIRE force o torque e la direzione su cui si vuole testare l'impulso 
+
+    /*if(count > 20 && count <= 23)
+      control_tau.tau.force.x = 100;        
+    else
+      control_tau.tau.force.x = 0; 
+    /*-----------------------------------------------------------------------*/
+
+
+    /*----------------------------SIMULAZIONE GRADINO -----------------------*/
+    //INSERIRE force o torque e la direzione su cui si vuole testare il gradino
+
+    /*if(count > 20)
+      control_tau.tau.force.x = 100;        
+    else
+      control_tau.tau.force.x = 0; 
+    /*-----------------------------------------------------------------------*/
+
+
+    /*---------------------VALIDAZIONE UNDER_WATER GPS-----------------------*/
+    /*if((count > 10 && count <= 15) || (count > 80 && count <=85))
+      control_tau.tau.force.z = 50;
+    else
+      control_tau.tau.force.z = 0;*/
+    /*-----------------------------------------------------------------------*/
+
+
+    /*---------------------------VALIDAZIONE DVL-----------------------------*/
+    /*if(count > 10){
       control_tau.tau.force.x = 100;
-      
+      control_tau.tau.force.y = 100;
+    }
+    /*-----------------------------------------------------------------------*/
+
+
     control_pub.publish(control_tau);
-    
-
-
     loop_rate.sleep();
     count++;
   }
 
 return 0;
+
 }
